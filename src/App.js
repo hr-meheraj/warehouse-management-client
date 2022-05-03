5da417c9c18224f7e1b58233591570956c2b6cc9
hrmeheraj2021 <hrmeheraj2021@gmail.com>
2022-05-03 14:06:11 -0400

    Favicon Update

--- src/App.js
+++ src/App.js
@@ -1,58 +1,67 @@
-import './App.css';
-import Navbar from './components/Header/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Pages/Home/Home';
-import RequireAuth from './components/Authentication/RequireAuth';
-import MyItem from './components/Pages/MyItems/MyItem.jsx'
-import ManageInventory from './components/Pages/ManageInventory/ManageInventory';
-import Login from './components/Authentication/Login';
-import Register from './components/Authentication/Register';
-import ResetPassword from './components/Authentication/ResetPassword';
-import NotFound from './components/Pages/NotFound/NotFound';
-import AddNew from './components/Pages/AddNew/AddNew';
-import Blogs from './components/Pages/Blogs/Blogs';
-import Blog from './components/Pages/Blogs/Blog';
-import UpdateProducts from './components/Pages/ManageInventory/UpdateProducts';
-import { Toaster } from 'react-hot-toast'
+import "./App.css";
+import Navbar from "./components/Header/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Pages/Home/Home";
+import RequireAuth from "./components/Authentication/RequireAuth";
+import MyItem from "./components/Pages/MyItems/MyItem.jsx";
+import ManageInventory from "./components/Pages/ManageInventory/ManageInventory";
+import Login from "./components/Authentication/Login";
+import Register from "./components/Authentication/Register";
+import ResetPassword from "./components/Authentication/ResetPassword";
+import NotFound from "./components/Pages/NotFound/NotFound";
+import AddNew from "./components/Pages/AddNew/AddNew";
+import Blogs from "./components/Pages/Blogs/Blogs";
+import Blog from "./components/Pages/Blogs/Blog";
+import UpdateProducts from "./components/Pages/ManageInventory/UpdateProducts";
+import { Toaster } from "react-hot-toast";
 function App() {
-    return (
-        <div>
-
-            <Toaster
-                position="top-right"
-                reverseOrder={false}
-            />
-            <Navbar/>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/inventory' element={
-                    <RequireAuth>
-                        <ManageInventory/>
-                    </RequireAuth>
-                }/>
-                <Route path='/inventory/:id' element={
-                    <RequireAuth>
-                     <UpdateProducts/>
-                     </RequireAuth>
-
-                }/>
-                <Route path='/register' element={<Register/>}/>
-                <Route path='/login' element={<Login/>}/>
-                <Route path='/reset-password' element={<ResetPassword />} />
-                <Route path='/add-new' element={
-                    <RequireAuth><AddNew /></RequireAuth>
-                } />
-                <Route path='/blogs' element={<Blogs />} />
-                <Route path='/blogs/:id' element={<Blog/>}/>
-                <Route path='my-items' element={
-                    <RequireAuth>
-                        <MyItem />
-                    </RequireAuth>
-                }/>
-                <Route path='*' element={ <NotFound/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Toaster position="top-right" reverseOrder={false} />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/inventory"
+          element={
+            <RequireAuth>
+              <ManageInventory />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/inventory/:id"
+          element={
+            <RequireAuth>
+              <UpdateProducts />
+            </RequireAuth>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route
+          path="/add-new"
+          element={
+            <RequireAuth>
+              <AddNew />
+            </RequireAuth>
+          }
+        />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:id" element={<Blog />} />
+        <Route
+          path="my-items"
+          element={
+            <RequireAuth>
+              <MyItem />
+            </RequireAuth>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
