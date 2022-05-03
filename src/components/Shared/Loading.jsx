ae3a20343fd48e3612df3c4f978485355afed151
hrmeheraj2021 <hrmeheraj2021@gmail.com>
2022-05-03 13:35:47 -0400

    Loading Format

--- src/components/Shared/Loading.jsx
+++ src/components/Shared/Loading.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import "./Loading.css"
+import React from "react";
+import "./Loading.css";
 
-const Loading  = () => {
-    return(
-        <div className='loading'>
-            <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+const Loading = () => {
+    return (
+        <div className="loading">
+            <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Loading;
