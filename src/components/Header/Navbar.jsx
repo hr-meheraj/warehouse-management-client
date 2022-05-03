24fe785193a36a420d780d569314410d310124c4
hrmeheraj2021 <hrmeheraj2021@gmail.com>
2022-05-03 13:26:00 -0400

    Navbar Format

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { useAuthState } from 'react-firebase-hooks/auth';
-import {signOut} from 'firebase/auth'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
 import Loading from "../Shared/Loading";
 function Navbar() {
@@ -11,45 +11,79 @@ function Navbar() {
         <nav
             className={`lg:sm:px-[80px] px-[10%] shadow-md flex justify-between items-center py-[15px] relative sm:py-3`}
         >
-            {
-                loading && <Loading/>
-            }
+            {loading && <Loading />}
             <div className="logo">
-                <Link to="/" className='cursor-pointer'>
+                <Link to="/" className="cursor-pointer">
                     <h3 className="text-xl ">I/W Management</h3>
                 </Link>
             </div>
 
-            <div className={` z-[50] md:gap-[20px] md:static items-center md:h-[100%] md:bg-white md:text-black transition-all  flex  md:flex-row  
-                   ${toggle ? " absolute top-0 bottom-0 right-0 left-0  justify-center  items-center gap-[20px] text-white  bg-[#000000ed]  text-[18px] flex-col w-full h-screen " : "right-0 left-0  absolute top-[-990px]"}`}>
-                <NavLink onClick={() => setToggle(false)} className="navLink" to="/blogs">
+            <div
+                className={` z-[50] md:gap-[20px] md:static items-center md:h-[100%] md:bg-white md:text-black transition-all  flex  md:flex-row  
+                   ${toggle
+                        ? " absolute top-0 bottom-0 right-0 left-0  justify-center  items-center gap-[20px] text-white  bg-[#000000ed]  text-[18px] flex-col w-full h-screen "
+                        : "right-0 left-0  absolute top-[-990px]"
+                    }`}
+            >
+                <NavLink
+                    onClick={() => setToggle(false)}
+                    className="navLink"
+                    to="/blogs"
+                >
                     Blogs{" "}
                 </NavLink>
-                <NavLink onClick={() => setToggle(false)} className="navLink" to="/inventory">
+                <NavLink
+                    onClick={() => setToggle(false)}
+                    className="navLink"
+                    to="/inventory"
+                >
                     Manage Inventory
-                </NavLink>
-                {
-                    user &&  ( <NavLink onClick={() => setToggle(false)} className="navLink sm:hidden lg:inline" to="/my-items">
-                    My Items
-                    </NavLink>)
-                }
-                {
-                    user && (<NavLink onClick={() => setToggle(false)} className="navLink sm:hidden lg:inline" to="/add-new">
+        </NavLink>
+                {user && (
+                    <NavLink
+                        onClick={() => setToggle(false)}
+                        className="navLink sm:hidden lg:inline"
+                        to="/my-items"
+                    >
+                        My Items
+                    </NavLink>
+                )}
+                {user && (
+                    <NavLink
+                        onClick={() => setToggle(false)}
+                        className="navLink sm:hidden lg:inline"
+                        to="/add-new"
+                    >
                         Add Item
-                    </NavLink>)
-                }
-                {
-                    user ? <button className='btn bg-blue-800 text-white ' onClick={() => signOut(auth)}>Log Out </button> : <Link onClick={() => setToggle(false)} className="ml-[20px] btn text-center bg-blue-800 text-white" to="/login">
-                    Login
-                </Link>
-                }
+                    </NavLink>
+                )}
+                {user ? (
+                    <button
+                        className="btn bg-blue-800 text-white "
+                        onClick={() => signOut(auth)}
+                    >
+                        Log Out{" "}
+                    </button>
+                ) : (
+                        <Link
+                            onClick={() => setToggle(false)}
+                            className="ml-[20px] btn text-center bg-blue-800 text-white"
+                            to="/login"
+                        >
+                            Login
+                        </Link>
+                    )}
             </div>
 
-            <div className={`block z-[100] md:hidden cursor-pointer hover:text-[#00fff0] transition-all`} onClick={() => setToggle(!toggle)} >
+            <div
+                className={`block z-[100] md:hidden cursor-pointer hover:text-[#00fff0] transition-all`}
+                onClick={() => setToggle(!toggle)}
+            >
                 {toggle ? (
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        className={`h-[35px] w-[35px] hover:text-pink-800 ${toggle ? "text-white" : ""}`}
+                        className={`h-[35px] w-[35px] hover:text-pink-800 ${toggle ? "text-white" : ""
+                            }`}
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
@@ -64,7 +98,8 @@ function Navbar() {
                 ) : (
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            className={`h-[35px] w-[35px] hover:text-pink-800 ${toggle ? "text-white" : ""}`}
+                            className={`h-[35px] w-[35px] hover:text-pink-800 ${toggle ? "text-white" : ""
+                                }`}
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
